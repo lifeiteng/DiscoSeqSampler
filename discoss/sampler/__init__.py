58c00b4b1fbdd04804a110b9e085b58a137d8f23
Feiteng <lifeiteng0422@gmail.com>
2025-08-03 18:26:21 +0800

    Add BucketingSampler

--- discoss/sampler/__init__.py
+++ discoss/sampler/__init__.py
@@ -1,6 +1,12 @@
 from .constraint import TokenConstraint
 from .simple import SimpleCutSampler
 from .dynamic_bucketing import DynamicBucketingCutSampler
+from .bucketing import BucketingCutSampler
 
 
-__all__ = ["TokenConstraint", "SimpleCutSampler", "DynamicBucketingCutSampler"]
+__all__ = [
+    "TokenConstraint",
+    "SimpleCutSampler",
+    "BucketingCutSampler",
+    "DynamicBucketingCutSampler",
+]
